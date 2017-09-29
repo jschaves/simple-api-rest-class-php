f74bf51ea4a4bffa4bef2950a2e98cb336a06164
Juan <juan.cha63@gmail.com>
2017-09-29 11:27:43 +0200

    Update
    Add multiple querys

--- simple-api-rest-class.php
+++ simple-api-rest-class.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 /**
  * this class displays a Simple Api Rest query in json format.
  * @author Juan Chaves, juan.cha63@gmail.com
@@ -6,53 +6,61 @@
  * This program is free software; distributed under the artistic license.
  */
 class SimpleApiRestClass {
-	
-	public $login_user;
-	public $login_pass;
-	public $query;
-	
+   
+    public $login_user;
+    public $login_pass;
+    public $query;
+    private $get_key;
+   
     /**
      * Method public ReturnLogin.
-     * @return Method private _login 
+     * @return Method private _login
      */
     public function ReturnLogin() {
-		//if logging is ok
-		if(
-			$this->login_user === $this->db['user'] && 
-			md5($this->login_pass) === $this->db['pass']
-		) {
-			
-			return $this->_login();
-		//if loguin error
-		} else {
-			
-			return json_encode('[User or password is wrong]');
-		
-		}
-		
+        //if logging is ok
+        if(
+            $this->login_user === $this->db['user'] &&
+            md5($this->login_pass) === $this->db['pass']
+        ) {
+           
+            return $this->_login();
+        //if loguin error
+        } else {
+           
+            echo 'User or password is wrong';
+       
+        }
+       
     }
-   
+  
     /**
      * Method private _login.
      * @return the result in json format.
      */
     private function _login() {
 
-		return '{"' . $this->query . '":' . json_encode($this->db[$this->query]) . '}'; 
-		
+		foreach(explode(',', $this->query) as $q) {
+
+			$result[$q] = $this->db[$q];
+			
+		}
+       return  json_encode($result);
     }
-		
+       
 }
 //name user
 $array_login['user'] = 'admin';
 //pass - md5 example e10adc3949ba59abbe56e057f20f883e = 123456
 $array_login['pass'] = 'e10adc3949ba59abbe56e057f20f883e';
-//fields to consult example simple_api_rest_class.php?u=admin&p=123456&q=brand
+//fields to consult example simple_api_rest_class.php?u=admin&p=123456&q=brand,model,quantity
 //results brand : "Iphone, Samsung"
-$array_login['brand'] = 'Iphone, Samsung';
-$array_login['model'] = '{Iphone:[Iphone 5, Iphone 6]}, {Samsung:[Samsung Galaxy S6, Samsung Galaxy S7]}';
-$array_login['quantity'] = '{Iphone:{Iphone 5:3, Iphone 6:4}';
-
+$array_login['brand'] = array('Iphone', 'Samsung');
+$array_login['model']['Iphone'] = array('Iphone 5', 'Iphone 6');
+$array_login['model']['Samsung'] = array('Galaxy S6', 'Galaxy S7');
+$array_login['quantity']['Iphone']['Iphone 5'] = 20;
+$array_login['quantity']['Iphone']['Iphone 6'] = 15;
+$array_login['quantity']['Samsung']['Galaxy S6'] = 30;
+$array_login['quantity']['Samsung']['Galaxy S7'] = 55;
 
 
 
