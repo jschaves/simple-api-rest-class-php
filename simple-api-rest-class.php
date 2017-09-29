b4fe42782a2dd23f142a5edc1f67ec967f085fa7
Juan <juan.cha63@gmail.com>
2017-09-29 11:36:59 +0200

    Update simple-api-rest-class.php

--- simple-api-rest-class.php
+++ simple-api-rest-class.php
@@ -1,5 +1,4 @@
- <?php
- 
+<?php
 /**
  * this class displays a Simple Api Rest query in json format.
  * @author Juan Chaves, juan.cha63@gmail.com
@@ -74,5 +73,4 @@ $query->db = $array_login;
 
 header('Content-type: application/json');
 echo $query->ReturnLogin();
-
 ?>
